9a12dbdc9ebac1bd2d061cd97ff4da6a3244c61a
santizabe <santizabe@gmail.com>
2023-01-14 12:02:39 -0500

    merge challenge01 to main

---
+++ challenge01/script.js
@@ -0,0 +1,77 @@
+const displayText = (text) => {
+  const p = document.querySelector(".text");
+  p.innerHTML = text;
+};
+
+const encrypt = (e) => {
+  e.preventDefault();
+  let text = document.getElementById("text").value;
+  var newStr = "";
+  for (let i = 0; i < text.length; i++) {
+    switch (text.charAt(i)) {
+      case "a":
+        newStr += "ai";
+        break;
+      case "e":
+        newStr += "enter";
+        break;
+      case "i":
+        newStr += "imes";
+        break;
+      case "o":
+        newStr += "ober";
+        break;
+      case "u":
+        newStr += "ufat";
+        break;
+      default:
+        newStr += text.charAt(i);
+    }
+  }
+  const copy = document.getElementById("copy");
+  const message = document.querySelector(".message");
+  const pikachu = document.querySelector(".no-msg-found");
+  const span = document.querySelector(".span");
+  span.style.display = "none";
+  pikachu.style.display = "none";
+  message.style.display = "none";
+  copy.style.display = "block";
+  displayText(newStr);
+};
+
+const decrypt = (e) => {
+  e.preventDefault();
+  let text = document.getElementById("text").value;
+  if (text.includes("ai")) text = text.replaceAll("ai", "a");
+  if (text.includes("enter")) text = text.replaceAll("enter", "e");
+  if (text.includes("imes")) text = text.replaceAll("imes", "i");
+  if (text.includes("ober")) text = text.replaceAll("ober", "o");
+  if (text.includes("ufat")) text = text.replaceAll("ufat", "u");
+
+  const copy = document.getElementById("copy");
+  const message = document.querySelector(".message");
+  const pikachu = document.querySelector(".no-msg-found");
+  const span = document.querySelector(".span");
+  span.style.display = "none";
+  pikachu.style.display = "none";
+  message.style.display = "none";
+  copy.style.display = "block";
+  displayText(text);
+};
+
+const copy = async () => {
+  try {
+    const text = document.querySelector(".text");
+    navigator.clipboard.writeText(text.innerHTML);
+  } catch {
+    alert("Couldn't copy text");
+  }
+};
+
+const encryptText = document.querySelector(".encrypt");
+const decryptText = document.querySelector(".decrypt");
+const copyBtn = document.querySelector("#copy");
+
+encryptText.onclick = encrypt;
+decryptText.onclick = decrypt;
+copyBtn.onclick = copy;
